6f276282d1b7e03d10e24ed46fe2c44944e24ab3
Abdalftah Abu-Khurma <bat30970@gmail.com>
2025-08-16 21:26:38 +0300

    Add files via upload

---
+++ Problem_16/Main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void PrintWordsFromAAAtoZZZ() {
+    string Word = "";
+
+    for (int i = 65; i <= 90; i++) {
+        for (int j = 65; j <= 90; j++) {
+            for (int k = 65; k <= 90; k++) {
+
+                Word = Word + char(i) + char(j) + char(k);
+                cout << Word << endl;
+                Word = "";
+
+            }
+        }
+    }
+}
+
+int main() {
+    PrintWordsFromAAAtoZZZ();
+    return 0;
+}
---
+++ Problem_16/README.md
@@ -0,0 +1,4 @@
+### Problem 16
+Print all words from AAA to ZZZ.
+
+**Output:** AAA, AAB, ..., ZZZ
